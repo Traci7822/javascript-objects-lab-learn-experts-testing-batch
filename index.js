52fa46f71a3e4925a920656d8709f646923d6d18
Traci7822 <Tracir@gmail.com>
2017-01-13 10:21:08 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,21 @@
+var recipes = [];
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, { [key] : value});
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  var updatedObject = object;
+  delete updatedObject.key;
+  return updatedObject;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
+}
